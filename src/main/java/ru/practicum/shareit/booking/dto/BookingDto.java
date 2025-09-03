e201cbf84cc8508d03327063062a9fc5dcff5887
Nia <eugeniamashina@gmail.com>
2025-09-03 10:12:05 +0300

    add booking dto, status and model

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,7 +1,15 @@
 package ru.practicum.shareit.booking.dto;
 
-/**
- * TODO Sprint add-bookings.
- */
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class BookingDto {
+    private Long id;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private Long itemId;
+    private Long bookerId;
+    private String status;
 }
