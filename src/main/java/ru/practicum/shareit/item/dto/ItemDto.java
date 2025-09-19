4a5d09d1832bdf4a33e7e03f15a9253ea9f83af5
Maksim Torzhkov <maxtorj@yandex.ru>
2025-09-19 11:47:20 +0300

    feat: update Item to JPA

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.item.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,9 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ItemDto {
     private Long id;
-    @NotBlank(message = "Название предмета не может быть пустым")
     private String name;
-    @NotBlank(message = "Описание предмета не может быть пустым")
     private String description;
     private Boolean available;
     private Long ownerId;
