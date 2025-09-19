776765336de8f089cb9dfe577e1b9c6c84c348b0
Maksim Torzhkov <maxtorj@yandex.ru>
2025-09-19 11:53:02 +0300

    feat: update Booking to JPA

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,8 +1,5 @@
 package ru.practicum.shareit.booking.dto;
 
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.FutureOrPresent;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,13 +11,8 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class BookingDto {
     private Long id;
-    @NotNull(message = "Дата начала бронирования обязательна")
-    @FutureOrPresent(message = "Дата начала должна быть в будущем или настоящем")
     private LocalDateTime start;
-    @NotNull(message = "Дата окончания бронирования обязательна")
-    @Future(message = "Дата окончания должна быть в будущем")
     private LocalDateTime end;
-    @NotNull(message = "ID предмета обязателен")
     private Long itemId;
     private String status;
 }
