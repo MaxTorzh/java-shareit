e656b8ee1929724cd78bb4d529981b52227b0b82
Maksim Torzhkov <maxtorj@yandex.ru>
2025-10-03 13:49:34 +0300

    feat: update booking gateway module

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingClient.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingClient.java
@@ -45,4 +45,22 @@ public class BookingClient extends BaseClient {
     public ResponseEntity<Object> getBooking(long userId, Long bookingId) {
         return get("/" + bookingId, userId);
     }
+
+    public ResponseEntity<Object> approveBooking(long userId, Long bookingId, Boolean approved) {
+        Map<String, Object> parameters = Map.of("approved", approved);
+        return patch("/" + bookingId + "?approved={approved}", userId, parameters, null);
+    }
+
+    public ResponseEntity<Object> getOwnerBookings(long userId, BookingState state, Integer from, Integer size) {
+        Map<String, Object> parameters = Map.of(
+                "state", state.name(),
+                "from", from,
+                "size",  size
+        );
+        return get("/owner?state={state}&from={from}&size={size}", userId, parameters);
+    }
+
+    public ResponseEntity<Object> cancelBooking(long userId, Long bookingId) {
+        return delete("/" + bookingId, userId);
+    }
 }
